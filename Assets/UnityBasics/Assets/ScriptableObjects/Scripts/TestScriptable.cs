5c663222712a4780d02b8bd09df86a091911a216
VinayKashyap15 <vinay@outscal.com>
2019-05-25 17:01:49 +0530

    Scriptable Object example added

---
+++ Assets/UnityBasics/Assets/ScriptableObjects/Scripts/TestScriptable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableObject_Test
+{
+    public class TestScriptable : MonoBehaviour
+    {
+        public TestScriptableObject scriptableObject;
+        private void Start()
+        {
+            Debug.Log("Value passed in scriptable object : " +scriptableObject.value);
+        }
+    }
+}
---
+++ Assets/UnityBasics/Assets/ScriptableObjects/Scripts/TestScriptableObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu (fileName ="Example ScriptableObject",menuName ="Custom Objects/Example Scriptable Object",order = 1)]
+public class TestScriptableObject : ScriptableObject
+{
+    public int value;
+}
