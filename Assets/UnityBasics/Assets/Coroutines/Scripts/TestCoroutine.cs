63bb0b5b8bc4802581be4ad02d1307304cc61ea5
Vinay Kashyap <vinay@outscal.com>
2019-05-25 15:50:29 +0530

    Coroutine Example added

---
+++ Assets/UnityBasics/Assets/Coroutines/Scripts/TestCoroutine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coroutines_Test
+{
+    public class TestCoroutine : MonoBehaviour
+    {
+        private bool canShow = false;
+        private Coroutine coroutine;
+        void Start()
+        {
+
+            Debug.Log("Start method");
+            //StartCoroutine(CoroutineExample());            
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                canShow = !canShow;
+
+                if (canShow)
+                {
+                    if (coroutine == null)
+                        coroutine = StartCoroutine(CoroutineExample());
+                }
+                else
+                {
+                    if (coroutine != null)
+                        StopCoroutine(coroutine);
+                }
+            }
+        }
+
+        IEnumerator CoroutineExample()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(3f);
+                Debug.Log("Inside Coroutine, message appears after 3 seconds");
+            }
+        }
+    }
+}
