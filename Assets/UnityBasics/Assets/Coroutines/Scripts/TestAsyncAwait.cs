f14bdff3ceec88aaf29f699eb8eb9d14c03e8b9c
VinayKashyap15 <vinay@outscal.com>
2019-05-25 16:29:50 +0530

    Async Await example

---
+++ Assets/UnityBasics/Assets/Coroutines/Scripts/TestAsyncAwait.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coroutines_Test
+{
+    public class TestAsyncAwait
+    {
+        private bool canShow = false;
+        public async void TestAsyncMethod()
+        {
+            canShow = true;
+            while (canShow)
+            {
+                await new WaitForSeconds(3f);
+                Debug.Log("<color=red>Async method called of a non mono class.</color>");
+            }
+        }
+
+        public void StopAsyncMethod()
+        {
+            canShow = false;
+        }
+    }
+}
--- Assets/UnityBasics/Assets/Coroutines/Scripts/TestCoroutine.cs
+++ Assets/UnityBasics/Assets/Coroutines/Scripts/TestCoroutine.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿#define USE_ASYNC
+//#define USE_COROUTINE
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,29 +10,55 @@ namespace Coroutines_Test
     public class TestCoroutine : MonoBehaviour
     {
         private bool canShow = false;
+        private bool hasAsyncStarted = false;
         private Coroutine coroutine;
+        private TestAsyncAwait testAsyncAwait;
         void Start()
         {
 
             Debug.Log("Start method");
             //StartCoroutine(CoroutineExample());            
+#if USE_ASYNC
+            testAsyncAwait = new TestAsyncAwait();
+#endif
         }
 
         private void Update()
         {
+
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 canShow = !canShow;
 
                 if (canShow)
                 {
+#if USE_ASYNC
+                    if (!hasAsyncStarted)
+                    {
+                        hasAsyncStarted = true;
+                        testAsyncAwait.TestAsyncMethod();
+                    }
+#endif
+#if USE_COROUTINE
+
                     if (coroutine == null)
                         coroutine = StartCoroutine(CoroutineExample());
+#endif
+
                 }
                 else
                 {
+#if USE_ASYNC
+                    hasAsyncStarted = false;
+                    testAsyncAwait.StopAsyncMethod();
+
+#endif
+
+#if USE_COROUTINE
                     if (coroutine != null)
                         StopCoroutine(coroutine);
+#endif
                 }
             }
         }
