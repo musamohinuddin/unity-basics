467b67d9fb48bb66b476cef15ce7f19e652153e6
VinayKashyap15 <vinay@outscal.com>
2019-05-27 13:25:20 +0530

    Resources and Streaming Assets added

---
+++ Assets/UnityBasics/Assets/Loading/Scripts/TestResourceLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine;
+
+namespace Loading_Test
+{
+    public class TestResourceLoader : MonoBehaviour
+    {
+        string text;
+        string path = "Assets/UnityBasics/Assets/StreamingAssets/TestTextDoc.txt";        
+        void Start()
+        {
+            GameObject testGO= GameObject.Instantiate(Resources.Load("TestResource"),Vector3.zero,Quaternion.identity) as GameObject;          
+            string filedata=File.ReadAllText(path);
+            Debug.Log(filedata);
+        }
+        
+    }
+}
