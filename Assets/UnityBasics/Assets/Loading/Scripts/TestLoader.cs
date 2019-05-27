d68f9f34e35288becddc5ae1a38f77e098ed2b81
VinayKashyap15 <vinay@outscal.com>
2019-05-27 15:39:06 +0530

    Added texture loading from streaming assets

--- Assets/UnityBasics/Assets/Loading/Scripts/TestLoader.cs
+++ Assets/UnityBasics/Assets/Loading/Scripts/TestLoader.cs
@@ -1,21 +1,50 @@
-﻿using System;
+﻿#define USE_RESOURCES
+//Comment above one
+//Uncomment below one to load texture from Streaming assets
+//#define USE_STREAMING_ASSETS
+
+using System;
 using System.IO;
 using System.Collections;
 using UnityEngine.Networking;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 namespace Loading_Test
 {
     public class TestLoader : MonoBehaviour
     {
         string text;
-        string path = "Assets/UnityBasics/Assets/StreamingAssets/TestTextDoc.txt";        
+        string filepath = "Assets/UnityBasics/Assets/StreamingAssets/TestTextDoc.txt";
+        string path = "E:/Dev/Home_Dev/UnityBasicsModule/Assets/UnityBasics/Assets/StreamingAssets/TestTexture.jpg";
+        Texture2D texture;
+        GameObject testGO;
         void Start()
         {
-            GameObject testGO= GameObject.Instantiate(Resources.Load("TestResource"),Vector3.zero,Quaternion.identity) as GameObject;          
-            string filedata=File.ReadAllText(path);
+            testGO = GameObject.Instantiate(Resources.Load("TestResource"), Vector3.zero, Quaternion.identity) as GameObject;
+#if USE_RESOURCES
+
+            texture = Resources.Load("TestTexture") as Texture2D;
+            Sprite s = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            testGO.AddComponent<SpriteRenderer>();
+            testGO.GetComponent<SpriteRenderer>().sprite = s;
+#endif
+            string filedata = File.ReadAllText(filepath);
             Debug.Log(filedata);
+#if USE_STREAMING_ASSETS
+            StartCoroutine(LoadTexture());
+#endif
         }
-        
+        IEnumerator LoadTexture()
+        {
+            UnityWebRequest unityWeb = UnityWebRequestTexture.GetTexture(path);
+
+            yield return unityWeb.SendWebRequest();            
+            texture = DownloadHandlerTexture.GetContent(unityWeb);
+            Sprite s = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            testGO.AddComponent<SpriteRenderer>();
+            testGO.GetComponent<SpriteRenderer>().sprite = s;            
+        }
     }
 }
